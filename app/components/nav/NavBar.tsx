7d6f38785f3e0d33da2e5edf4942e8cc8a1db40c
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-08-15 00:47:36 -0300

    feat: add nav components

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -2,10 +2,13 @@ import Link from 'next/link';
 import Container from '../Container';
 import { Redressed } from 'next/font/google';
 import CartCount from './CartCount';
-
+import UserMenu from './UserMenu';
+import { getCurrentUser } from '@/actions/getCurrentUser';
 const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
 
-const NavBar = () => {
+const NavBar = async () => {
+  const currentUser = await getCurrentUser();
+
   return (
     <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -20,7 +23,7 @@ const NavBar = () => {
             <div className="hidden md:block">Pesquisar</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <div>UserMenu</div>
+              <UserMenu currentUser={currentUser} />
             </div>
           </div>
         </Container>
